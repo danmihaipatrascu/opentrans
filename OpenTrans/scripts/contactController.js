55eabdf65146c54adb0f05f33ee40332dbf32663
BENI <beni_titoc@yahoo.com>
2017-01-28 20:19:34 +0200

    contact

--- OpenTrans/scripts/contactController.js
+++ OpenTrans/scripts/contactController.js
@@ -5,15 +5,26 @@
         emailjs.init("user_VOtzoEw2lKT8vCO1OcQXr");
     };
     initEmailJs();
+    vm.showErrorAlert = false;
+    vm.showSuccesAlert = false;
 
     var _send = function () {
+        vm.showSuccesAlert = true; //TODO : handle response
         emailjs.send("opentransservice", "opentransemailtemplate",
-            {
-                name: vm.name,
-                email: vm.email,
-                phone: vm.phone,
-                message: vm.message
-            });
+              {
+                  name: vm.name,
+                  email: vm.email,
+                  phone: vm.phone,
+                  message: vm.message
+              }).then(
+
+              function (data) {
+                  vm.showSuccesAlert = true;
+
+              }, function (response) {
+                  vm.showErrorAlert = true;
+              }
+            );
     }
 
     vm.send = _send;
