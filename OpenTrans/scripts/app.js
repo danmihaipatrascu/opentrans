b30762f6aa7b70bec849206d92623032f3dc1d72
Dan <danmihaipatrascu@gmail.com>
2017-02-27 13:29:21 +0200

    deploy fixes

--- OpenTrans/scripts/app.js
+++ OpenTrans/scripts/app.js
@@ -65,3 +65,21 @@ myApp.controller('HomeCtrl', function ($scope) {
 myApp.controller('AboutCtrl', function ($scope) {
     $scope.message = "(',')---I am on About page---(',')";
 });
+
+myApp.directive('scrollOnClick', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, $elm, attrs) {
+            var idToScroll = attrs.href;
+            $elm.on('click', function () {
+                var $target;
+                if (idToScroll) {
+                    $target = $(idToScroll);
+                } else {
+                    $target = $elm;
+                }
+                $("body").animate({ scrollTop: $target.offset().top - 100 }, "slow");
+            });
+        }
+    }
+});
